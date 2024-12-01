e8f474f7e719db0137d1f6cdced701c85b2453ba
DonKingDingaLing <dominik-gruber@live.de>
2024-12-01 16:01:07 +0100

    Solved challenge 2 day 1 in java

--- aoc-2024/day_1/challenge_2/java_workspace/src/main/java/Challenge2Day1.java
+++ aoc-2024/day_1/challenge_2/java_workspace/src/main/java/Challenge2Day1.java
@@ -1,5 +1,31 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Objects;
+
 public class Challenge2Day1 {
-    public static void main(String[] args) {
-        System.out.println("Start here!");
+    public static void main(String[] args) throws IOException {
+        var inputFile = new File("aoc-2024/day_1/challenge_2/input.txt");
+        var inputReader = new BufferedReader(new FileReader(inputFile));
+
+        var firstList = new ArrayList<Integer>();
+        var secondList = new ArrayList<Integer>();
+
+        var currentLine = "";
+        while((currentLine = inputReader.readLine()) != null) {
+            var values = currentLine.split("   ");
+            firstList.add(Integer.valueOf(values[0]));
+            secondList.add(Integer.valueOf(values[1]));
+        }
+
+        Collections.sort(firstList);
+        Collections.sort(secondList);
+
+        var similarities = firstList.stream()
+                .map(x -> secondList.stream().filter(y -> Objects.equals(x, y)).count() * x)
+                .toList();
+
+        var result = similarities.stream().mapToInt(Long::intValue).sum();
+        System.out.println(result);
     }
 }
