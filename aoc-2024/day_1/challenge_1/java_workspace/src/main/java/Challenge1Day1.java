b8c0df06c30f62b866f5d1977e9b22b718e40936
DonKingDingaLing <dominik-gruber@live.de>
2024-12-01 15:45:07 +0100

    Solved challenge 1 day 1 in java

--- aoc-2024/day_1/challenge_1/java_workspace/src/main/java/Challenge1Day1.java
+++ aoc-2024/day_1/challenge_1/java_workspace/src/main/java/Challenge1Day1.java
@@ -1,5 +1,31 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Challenge1Day1 {
-    public static void main(String[] args) {
-        System.out.println("Start here!");
+    public static void main(String[] args) throws IOException {
+        var inputFile = new File("aoc-2024/day_1/challenge_1/input.txt");
+        var inputReader = new BufferedReader(new FileReader(inputFile));
+
+        var firstList = new ArrayList<Integer>();
+        var secondList = new ArrayList<Integer>();
+        var distance = new ArrayList<Integer>();
+
+        var currentLine = "";
+        while((currentLine = inputReader.readLine()) != null) {
+            var values = currentLine.split("   ");
+            firstList.add(Integer.valueOf(values[0]));
+            secondList.add(Integer.valueOf(values[1]));
+        }
+
+        Collections.sort(firstList);
+        Collections.sort(secondList);
+
+        for(int i = 0; i < firstList.size(); i++) {
+            distance.add(Math.abs(firstList.get(i) - secondList.get(i)));
+        }
+
+        var result = distance.stream().mapToInt(Integer::intValue).sum();
+        System.out.println(result);
     }
 }
