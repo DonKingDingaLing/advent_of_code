b8e53a84d8a3b1e5d81bc1fd8959e6faaeae5dd3
DonKingDingaLing <dominik-gruber@live.de>
2024-12-04 22:07:44 +0100

    Solved challenge 1 day 2

--- aoc-2024/day_2/challenge_1/java_workspace/src/main/java/Challenge1Day2.java
+++ aoc-2024/day_2/challenge_1/java_workspace/src/main/java/Challenge1Day2.java
@@ -1,5 +1,37 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Challenge1Day2 {
-    public static void main(String[] args) {
-        System.out.println("Start here!");
+    public static void main(String[] args) throws IOException {
+        var inputFile = new File("aoc-2024/day_2/challenge_1/input.txt");
+        var inputReader = new BufferedReader(new FileReader(inputFile));
+
+        var validReports = inputReader.lines()
+                .map(currentLine -> Arrays.stream(currentLine.split(" "))
+                    .map(Integer::valueOf)
+                    .toList())
+                .filter(report -> {
+                    Boolean lastDirection = null;
+                    for(int i = 0; i < report.size() - 1; i++) {
+                        var distance = report.get(i)-report.get(i+1);
+                        var increasing = distance < 0;
+                        distance = Math.abs(distance);
+
+                        if((lastDirection != null && lastDirection != increasing)
+                                || (distance < 1 || distance > 3)) {
+                            return false;
+                        }
+
+                        lastDirection = increasing;
+                    }
+                    return true;
+                })
+                .count();
+
+        System.out.println(String.format("Result: %s", validReports));
     }
 }
