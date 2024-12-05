a8f9c0be27bb91639e005fc55bd8e74f449388be
DonKingDingaLing <dominik-gruber@live.de>
2024-12-05 22:24:07 +0100

    Solved challenge 2 day 2

--- aoc-2024/day_2/challenge_2/java_workspace/src/main/java/Challenge2Day2.java
+++ aoc-2024/day_2/challenge_2/java_workspace/src/main/java/Challenge2Day2.java
@@ -1,5 +1,55 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Challenge2Day2 {
-    public static void main(String[] args) {
-        System.out.println("Start here!");
+    public static void main(String[] args) throws FileNotFoundException {
+        var inputFile = new File("aoc-2024/day_2/challenge_2/input.txt");
+        var inputReader = new BufferedReader(new FileReader(inputFile));
+
+        var validReports = inputReader.lines()
+                .map(currentLine -> Arrays.stream(currentLine.split(" "))
+                    .map(Integer::valueOf)
+                    .toList())
+                .filter(Challenge2Day2::isSafe)
+                .count();
+
+        System.out.println(String.format("Result: %s", validReports));
+    }
+
+    public static boolean isSafe(List<Integer> report) {
+        if(isSafePermutation(report)) {
+            return true;
+        }
+
+        for(int i = 0; i < report.size(); i++) {
+            var permutation = new ArrayList<>(report);
+            permutation.remove(i);
+            if(isSafePermutation(permutation))
+                    return true;
+        }
+        return false;
+    }
+
+    public static boolean isSafePermutation(List<Integer> report) {
+        Boolean lastDirection = null;
+        for (int i = 0; i < report.size() - 1; i++) {
+            var distance = report.get(i) - report.get(i + 1);
+            var increasing = distance < 0;
+            distance = Math.abs(distance);
+            var directionInconsistent = lastDirection != null && lastDirection != increasing;
+            var distanceInconsistent = distance < 1 || distance > 3;
+
+            if (distanceInconsistent || directionInconsistent) {
+                return false;
+            }
+
+            lastDirection = increasing;
+        }
+        return true;
     }
 }
